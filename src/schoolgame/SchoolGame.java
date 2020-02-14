928f2ae25ef56726f2c8aba4b7b5759cd321bb9d
14cfirth <60881816+14cfirth@users.noreply.github.com>
2020-02-14 11:40:39 +0000

    Updated

--- src/schoolgame/SchoolGame.java
+++ src/schoolgame/SchoolGame.java
@@ -7,6 +7,8 @@ package schoolgame;
 
 import schoolgame.Engine.GameEngine;
 import schoolgame.Game.BallComponent;
+import schoolgame.Game.DemoComponent;
+import schoolgame.Game.GameController;
 import schoolgame.Models.GameObject;
 import schoolgame.Models.MotionComponent;
 
@@ -21,14 +23,22 @@ public class SchoolGame {
      */
     public static void main(String[] args) throws InterruptedException {
         new GameEngine();
-        new GameObject("bg", 400, 400, 0, "/schoolgame/resources/background.png", false);
-        GameObject go = new GameObject("debug", 400, 400, 1, "/schoolgame/resources/debug.png", true, new BallComponent());
-        GameObject centre = new GameObject("point", 400, 400, 2, "/schoolgame/resources/point.png", false);
+        new GameObject("bg", 0, 0, 0, "/schoolgame/resources/background.png", false);
+        /*
+                GameObject go = new GameObject("debug", 200, 200, 1, "/schoolgame/resources/ball.png", true, new BallComponent());
+        GameObject g1 = new GameObject("debug", 200, 200, 1, "/schoolgame/resources/box.png", true, new BallComponent());
+        GameObject g2 = new GameObject("debug", 200, 200, 1, "/schoolgame/resources/cannon.png", true, new BallComponent());
+        go.AddMotion(new MotionComponent(3, 3, 300));
+        g1.AddMotion(new MotionComponent(1, 2, 300));
+        g2.AddMotion(new MotionComponent(4, 8, 300));
         for (int i = 0; i <= 360; i++) {
             go.rotation = i;
             Thread.sleep(10);
         }
-        go.AddMotion(new MotionComponent(3, 3, 300));
+                */
+        new GameController();
+        
+        GameController.singleton.Start();
     }
     
 }
