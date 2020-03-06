2f52364033de78001ce9a053f9a0121c54c1c089
14cfirth <60881816+14cfirth@users.noreply.github.com>
2020-03-06 11:50:08 +0000

    Fixed Domain for base rotation

--- src/schoolgame/Game/BaseComponent.java
+++ src/schoolgame/Game/BaseComponent.java
@@ -78,9 +78,9 @@ public class BaseComponent implements IGameObjectComponent, IKeyCallback {
 
     @Override
     public void KeyPress(KeyEvent ke) {
-        if (ke.getKeyCode() == 37) { //Left
+        if (ke.getKeyCode() == 37 && (Math.abs(me.rotation) <= 88 || me.rotation >= 80)) { //Left
             me.rotation -= 2;
-        } else if (ke.getKeyCode() == 39) { //Right
+        } else if (ke.getKeyCode() == 39 && (Math.abs(me.rotation) <= 88 || me.rotation <= -80)) { //Right
             me.rotation += 2;
         } else if (ke.getKeyCode() == 38 || ke.getKeyCode() == 32) { //Up or space
             if (!GameController.singleton.canFire || Math.abs(me.rotation) > 89) return;
